169a2ee61eeb7601044d129f8ce59db8d315758f
Tim Kosem <tim.kosem@ey.com>
2018-03-02 13:26:47 -0500

    enabled VS Code debugging of SSR configuration

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -19,6 +19,7 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js'
   },
+  devtool: 'source-map',
   module: {
     rules: [
       { test: /\.ts$/, loader: 'ts-loader' }
